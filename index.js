c17cf1caaf858ddf5e976fd70cafc7daf8fc9984
mamun-6amtech <mamun.6amtech@gmail.com>
2024-07-09 13:46:48 +0600

    fix: change for ssl

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
-const { Server } = require('socket.io');
+const {fs} = require('node:fs');
+const {https} = require('node:https');
+const {createServer} = require('node:http');
+const {join} = require('node:path');
+const {Server} = require('socket.io');
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
+// const server = createServer(app);
+// const io = new Server(server);
+const options = {
+    key: fs.readFileSync('/path/to/your/private.key'),
+    cert: fs.readFileSync('/path/to/your/certificate.crt')
+};
 
+const server = https.createServer(options, app);
+const io = new Server(server);
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'public/index.html'));
 });
